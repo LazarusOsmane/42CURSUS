3dbd995d97bdc7196ad5220f8a7f53f28fcba72a
lazarusosmane <lazarusosmane@gmail.com>
2021-10-06 00:44:08 +0200

    libft version 2.0

---
+++ ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: engooh <erickngooh.1@gmail.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/01 21:39:42 by engooh            #+#    #+#             */
+/*   Updated: 2021/10/01 21:51:46 by engooh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*p;
+
+	p = malloc(sizeof(t_list));
+	if (!p)
+		return (NULL);
+	p->next = NULL;
+	p->content = content;
+	return (p);
+}
