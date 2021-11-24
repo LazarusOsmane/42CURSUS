d8ae8387cc8725c3d2aed9ccffa7e76522a399c6
Erick NGOOH <engooh@e3r12p3.42.fr>
2021-11-24 12:09:48 +0100

    libft v4

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: engooh <erickngooh.1@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 03:47:08 by engooh            #+#    #+#             */
-/*   Updated: 2021/09/26 18:38:21 by engooh           ###   ########.fr       */
+/*   Updated: 2021/11/24 12:02:52 by engooh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,7 +16,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	d;
 
 	d = 0;
-	while (d < n)
+	while (d < n && (dest || src))
 		*((unsigned char *)(dest + d++)) = *((unsigned char *)src++);
 	return (dest);
 }
--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,16 +6,19 @@
 /*   By: engooh <erickngooh.1@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 23:34:00 by engooh            #+#    #+#             */
-/*   Updated: 2021/09/26 11:20:13 by engooh           ###   ########.fr       */
+/*   Updated: 2021/11/24 12:08:00 by engooh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <string.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char	*d;
 
 	d = dest;
+	if (!dest && !src)
+	    return (NULL);
 	if (d > (unsigned char *)src)
 		while (n--)
 			*(d + n) = *((unsigned char *)src + n);
@@ -23,4 +26,10 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 		while (n--)
 			*d++ = *((unsigned char *)src++);
 	return (dest);
+}
+
+int	main(void)
+{
+
+    return (0);
 }
