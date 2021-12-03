90208c182d85fc3ea76edce66e3c7c68f22e679f
Erick NGOOH <engooh@e3r12p2.42.fr>
2021-12-03 19:45:43 +0100

    merci anis && bagdadi

--- get_next_line/dingueriesaaahencoreplus.c
+++ get_next_line/dingueriesaaahencoreplus.c
@@ -4,7 +4,7 @@
 #include <fcntl.h>
 
 
-int	ft_strchr(char *str, int *n, char *str1)
+int	ft_strchr(char *str, char *str1)
 {
 	int	i;
 	int	ok;
@@ -32,7 +32,7 @@ char get_next_line(int fd)
 {
 	static char	*str = malloc(sizeof(char) * (10000000 + 1)); 
 	int		len_max;
-	char		*line;
+	char	*line;
 	int		len;
 	int		back_slash_n;
 
@@ -40,12 +40,12 @@ char get_next_line(int fd)
 	line = str;
 	len_max = 0;
 	back_slash_n = 0;
-	while (!ft_strchr(str + (len_max - len), '\n', &back_slash_n, str + len_max) && len)
+	while (!ft_strchr(str + (len_max - len), str + len_max, &back_slash_n) && len)
 	{
 		len = read(fd, str + len_max, BUFFER_SIZE);
 		len_max += len;
 	}
-	str += back_slash_n + len_max - len + 1);
+	str += back_slash_n + len_max - len + 1;
 	*str++ = '\0';
 	return (line);
 }
@@ -53,19 +53,11 @@ char get_next_line(int fd)
 
 int main(void)
 {
-	static char	*str;
-	int	n;
-	int	fd = open("panda", O_RDONLY);
+	int	fd;
 
-	n = 0;
-	str = calloc(sizeof(char) * (BUFFER_SIZE * 2));
-	n = read(fd, str + n, BUFFER_SIZE);
-	printf("%s\n", str);
-	*(str + n) = '\0';
-	int b = n;
-	char	*line = str;
-	n += read(fd, str + n + 1, BUFFER_SIZE);
-	printf("%s\n", line);
-	printf("str 2 = %s", str + b + 1);
+	fd = open("bible.txt", O_RDONLY);
+	if (!fd)
+		return (NULL);
+	printf("%s", get_next_line(fd));
 	return (0);
 }
--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: engooh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 14:58:54 by engooh            #+#    #+#             */
-/*   Updated: 2021/12/02 17:03:56 by engooh           ###   ########.fr       */
+/*   Updated: 2021/12/03 19:44:54 by engooh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -14,40 +14,48 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-char	*ft_strchr(char *str, int c)
+
+
+
+
+int	ft_strchr(char *str, int *n)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] && str[i] != c)
+	while (str[i] && str[i] != '\n')
 		i++;
-	if (str[i] != c)
-		return (NULL);
-	return (str);
+	if (str[i] == '\n')
+		*n = i;
+	else 
+		n = 0;
+	return (n);
 }
 
-char	*ft_strjoin(char *dst, char *src, char size)
-{
-	char	*new;
-	char	*c;
 
-	if (!dst)
-	{
-		dst = (char *)malloc(sizeof(char));
-		if (!dst)
-			return (NULL);
-		*dst = '\0';
-	}
-	new = malloc(sizeof(char) * (size + 1));
-	if (!new || !src)
+char	*ft_strjoin(char *dst, char *new, char size)
+{
+	char	*new_line;
+	size_t	i;
+
+	if (!buffer)
 		return (NULL);
-	c = new;
-	while (*dst)
-		*new++ = *dst++;
-	while (*src)
-		*new++ = *src++;
-	*new = '\0'; 
-	return (c);
+	if (!line)
+	{
+		line = malloc(sizeof(char));
+		if (!line)
+			return (NULL);
+		line[0] = '\0';
+	}
+	new_line = malloc(sizeof(char) * (size));
+	if (!new_line)
+		return (NULL);
+	while (*buffer && buffer != '\n')
+		*new_line++  = *buffer++;
+	if (*buffer == '\n')
+		*memory = buffer;
+	free(line);
+	return (new_line);
 }
 
 char	*ft_check_read(char *memory, int err)
@@ -60,37 +68,44 @@ char	*ft_check_read(char *memory, int err)
 	return (memory);
 }
 
-char	*ft_read(char **memory, int fd, int *n)
+char	*ft_read(char **memory, int fd)
 {
-	char	buffer[BUFFER_SIZE];
-	int	l_buf;
-	int	l_max;
-	char	line;
+	char	*buffer;
+	int		l_buf;
+	int		l_max;
+	char	*line;
+	int		n;
 
-
+	n = 0;
 	l_buf = 1;
 	l_max = 0;
-	while (!ft_strchr(buffer, '\n') && l_buf)
+	line = NULL;
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	while (!ft_strchr(memory + (l_max - l_buf), &n) && l_buf)
 	{
 		l_buf = read(fd, buffer, BUFFER_SIZE);
+		if (l_max == 0)
+			l_max += ft_strlen(memory);
 		l_max += l_buf;
-		buffer[l_buf] = '\0';
-		//printf("%s", buffer);
-		*memory = ft_strjoin(*memory, buffer, l_max);
-		//printf("%s", memory);
+		buffer[l_buf + 1] = '\0';
+		if (n)
+			*memory = ft_strjoin(*memory, buffer, (l_max - l_buf) + n);
+		else
+			*memory = ft_strjoin(*memory, buffer, l_max);
+
 	}
-	//printf("%s", memory);
-	//printf("%s", memory);
+	line = ft_strdup(memory, l_max - n);
 	return (ft_check_read(*memory, l_buf));
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*memory[1024];
+	char		*line;
 
-	ft_read(&memory[fd], fd, &n);
-	printf("##%d %s##", n, memory[fd] + n);
-	return (memory[fd]);
+	//printf("ok1");
+	line = ft_read(&memory[fd], fd);
+	return (line);
 }
 
 int	main(int argc, char **argv)
@@ -104,39 +119,8 @@ int	main(int argc, char **argv)
 	fd = open(argv[1], O_RDONLY);
 	if (!fd)
 		return (0);
-	printf("\n%s", get_next_line(fd));
+	//printf("ok");
+	get_next_line(fd);
+	//printf("\n%s", get_next_line(fd));
 	return (0);
 }
-
-/*char	*ft_strjoin(line, buffer + n, memory, start)
-{
-	tmp = malloc(start)
-	while (buffer != '\n')
-		*tmp++ = *buffer++;
-		start++;
-	//memory = malloc( start - buffer + n - line )
-	memory = buffer;
-	tmp2 = buffer
-	if (buffer != \0)
-		*memory++ = *buffer++
-	free(line)
-	return (tmp)
-}*/
-
-while (buffer)
-{
-	if (buffer - 1 == '\n')
-		line = line->next
-
-			
-	line = memory;
-
-	memory++ != '\n'
-
-	*memory = '\n' = n
-	memory++
-	*memory = \0
-	memory++
-
-}
-
